57eb7466ec36ba69ffe2c34ed87b0dfa973afde2
dfinke <finked@hotmail.com>
2018-11-07 13:45:50 -0500

    Fix so the doc generation only happens if the chapters exist

--- ConvertFromMarkdown.psm1
+++ ConvertFromMarkdown.psm1
@@ -1,6 +1,14 @@
 . $PSScriptRoot\exportManuscript.ps1
 . $PSScriptRoot\TestPSCodeBlock.ps1
 
+function Test-Chapter {
+    param (
+        $outputPath = $pwd
+    )
+
+    (Get-ChildItem $outputPath).Count -gt 0
+}
+
 function ConvertFrom-Markdown {
     param (
         $markdownFile = "$pwd\README.md",
@@ -13,39 +21,44 @@ function ConvertFrom-Markdown {
     Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Creating chapters"
     Export-Manuscript -markdownFile $markdownFile -outputPath $outputPath
 
+    $targetPath = "$($outputPath)\manuscript"
+
     Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Analyzing PowerShell"
     Test-PSCodeBlock -markdownFile $markdownFile -outputPath $outputPath
 
+
+    if (!(Test-Chapter $targetPath)) {
+        "No chapters found"
+        return
+    }
 
     #if ((Get-Command pandoc.exe -ErrorAction SilentlyContinue) -and $AsPDF) {
     if ($OutputType) {
 
         if (!(Get-Command pandoc.exe -ErrorAction SilentlyContinue)) {
             Write-Warning @"
-To generate that output tyype, you need to install Pandoc, https://pandoc.org/installing.html
+To generate that output type, you need to install Pandoc, https://pandoc.org/installing.html
 If you want a PDF, you need to also install LaTeX, https://miktex.org/
 "@
             return
         }
 
-        $targetPath = "$($outputPath)\manuscript"
-
         # $chapters = (Get-Content "$targetPath\book.txt") -join ' '
         $chapters = (Get-ChildItem $targetPath chap* | ForEach-Object FullName ) -join ' '
 
-        if ($chapters.trim().length -gt 0) {
-            $outFile = "$($targetPath)\book.$($OutputType)"
-            Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Creating $($OutputType)"
-            "pandoc $chapters -S --toc --standalone -o $outFile" | Invoke-Expression
+        # if ($chapters.trim().length -gt 0) {
+        $outFile = "$($targetPath)\book.$($OutputType)"
+        Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Creating $($OutputType)"
+        "pandoc $chapters -S --toc --standalone -o $outFile" | Invoke-Expression
 
-            Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Launching $($OutputType)"
-            if ($Show) {
-                Invoke-Item $outFile
-            }
+        Write-Progress -Activity "Generating manuscript" -Status "[$(Get-Date)] Launching $($OutputType)"
+        if ($Show) {
+            Invoke-Item $outFile
         }
-        else {
-            "No chapters found"
-        }
+        # }
+        # else {
+        #     "No chapters found"
+        # }
     }
 }
 
